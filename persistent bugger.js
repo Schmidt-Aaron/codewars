8495e898c86bfd7398a5a614f920b5dbcfcd592c
schmidt-aaron <aschmidt808@gmail.com>
2018-11-03 19:35:51 -0700

    added top solution

--- persistent bugger.js
+++ persistent bugger.js
@@ -34,4 +34,20 @@ const persistence = num => {
   return count;
 };
 
-// other solutions of note
+// top solution
+function persistence(num) {
+  var times = 0;
+
+  num = num.toString();
+
+  while (num.length > 1) {
+    times++;
+    num = num
+      .split("")
+      .map(Number)
+      .reduce((a, b) => a * b)
+      .toString();
+  }
+
+  return times;
+}
